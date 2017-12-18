ce97e4c727b171ad58b9c5d7c27b2d64b7122b96
Kimnd <fionagallag@gmail.com>
2017-12-17 18:49:23 -0800

    Print All the Things
    I added some explanatory comments (mostly for what the methods do), and some print statements (commented out) to better visualize what's going on in the computer as it runs the code if the student wants.

--- Recursion/stupid.java
+++ Recursion/stupid.java
@@ -1,40 +1,59 @@
+//If you want to see what's 'really' going on, uncomment the print statements, or add some yourself!
 public class stupid{
+//This method just lets me print stuff without writing "System.out.println();" five million times
+	public static void print(String text) {
+		System.out.print(text);
+	}
 
+//A method that figures out the 'n'th fibonacci number via self-reference
 	public static void stu(int n){
-		if(n <= 1){
-			System.out.println(n);
+//		print(" [n:" + n + "] ");
+		if(n <= 1) {
 			return;
 		}
-		stu(n-1);
-		stu(n-2);
-		stu(n-3);
+//		print("\nstu(n - 1): ");
+		stu(n - 1);
+//		print("\n    stu(n - 2): ");
+		stu(n - 2);
+//		print("\n        stu(n - 3): ");
+		stu(n - 3);
 	}
 
+//This method returns the 'n'th fibonacci number too, but doesn't reference itself to do so
 	public static int fib(int n){
-			if(n <=2 )
-				return 1;
-			int first = 1;
-			int second = 1;
-			int next = 0;
-			for(int  i = 3; i <= n; i++){
-				next = first+second;
-				first = second;
-				second = next;
-			}
-			return next;
+		print(" [n at start:" + n + "] ");
+		if(n <= 2) {
+			print(" (n <= 2, returns 1) ");
+			return 1;
+		}
+		int first = 1;
+		int second = 1;
+		int next = 0;
+		for(int i = 3; i <= n; i++) {//this for loop is what does the heavy lifting for our non-recursive method
+//			print(" (i:" + i + ") FIRST: " + first + " SECOND: " + second);
+			next = first+second;
+			first = second;
+			second = next;
+//			print(" NEXT: " + next + " --> ");
+		}
+		print(" Final return: " + next + "\n");
+		return next;
 	}
 
+//This method calls itself and eventually returns the nth fibonacci number
 	public static int fibrec(int n){
-		if(n <= 2)
+		print("\n    [n at start:" + n + "] ");
+		if(n <= 2) {
+			print(" (n <= 2, return 1) ");
 			return 1;
+		}
+		print(" Final return: " + fibrec(n - 1) + fibrec(n - 2) + "\n");
 		return fibrec(n-1)+fibrec(n-2);
 	}
 
 	public static void main(String[] args){
-		//stu(5);
-		System.out.println(fib(6));
-		System.out.println(fibrec(6));
+		stu(6);
+		print("\nNo recursion:   " + fib(6));
+		print("\n\nWith recursion: " + fibrec(6));
 	}
-
 }
-		
